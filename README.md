48008ce4c511f1d674f722a5a77cd073eeb5d719
LautaroLeivaLeyes <110570815+LautaroLeivaLeyes@users.noreply.github.com>
2023-03-17 12:04:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# diaria
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-rl7dcs)
---
+++ diariamente/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogoComponent,
+    HeaderComponent,
+    NoticiasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
